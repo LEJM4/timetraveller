8881e349e36433883c5ce0622f5f2bb7584bbd3f
Leopold R. <LEJM4@gmx.de>
2023-10-21 20:47:55 +0200

    Map eingefuegt

--- level.py
+++ level.py
@@ -1,4 +1,5 @@
 import pygame 
+from pytmx.util_pygame import load_pygame
 from settings import *
 from player import Player
 from bush import Bush
@@ -8,6 +9,9 @@ class Level:
     def __init__(self):
         #display_surface
         self.display_surface = pygame.display.get_surface()
+        self.backround = load_pygame('map/test.tmx')
+
+        
 
         # sprite groups
         self.all_sprites = pygame.sprite.Group()
@@ -20,5 +24,8 @@ class Level:
 
     def run(self,dt):
         self.display_surface.fill('black')
+        image = self.backround.get_tile_image(0, 0, 0)
+        self.display_surface.blit(image, (0,0))
+
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(dt)
