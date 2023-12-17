480d60ae37fbb8788ce2da1d9f645445144e2443
Leopold R <LEJM4@gmx.de>
2023-12-17 20:01:32 +0100

    Update bush.py

--- bush.py
+++ bush.py
@@ -8,7 +8,8 @@ class Bush(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
 
-        # self.frame_index_bush = 1 
+        # self.frame_index_bush = 1
+        self.bush_empty = False 
         self.image = pygame.image.load('graphics/objects/bush/blueberry.png').convert_alpha()
         self.rect = self.image.get_rect(center=pos) 
 
