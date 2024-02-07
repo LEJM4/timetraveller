ee0ae3da84f7baa9503309013ca104d8391af33d
Mynameishose <www.masterofhose@gmail.com>
2024-02-07 17:06:00 +0100

    Baum hinzugefuegt

--- map/trees.tsx
+++ map/trees.tsx
@@ -2,6 +2,6 @@
 <tileset version="1.10" tiledversion="1.10.2" name="trees" tilewidth="256" tileheight="256" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="256" height="256" source="../graphics/objects/tree/tree_2.png"/>
+  <image width="256" height="256" source="../graphics/objects/tree/tree.png"/>
  </tile>
 </tileset>
