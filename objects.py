40ccd161b008ee041180bafd46d597e3483d9f26
LEJM4 <LEJM4@gmx.de>
2024-02-17 22:01:43 +0100

    hinzufuegen neuer Class --> "Item" mit der soll man spaeter unterscheiden, welche Art von Object der Spieler aufnimmt. Spaeter vielleicht mal Muenzen

--- objects.py
+++ objects.py
@@ -10,15 +10,24 @@ class Tree(pygame.sprite.Sprite):
 
 
 class Bush(pygame.sprite.Sprite):
-    def __init__(self, pos, image, groups):
+    def __init__(self, pos, image, groups, item_type):
         super().__init__(groups)
         self.image = image
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect
+        self.item_type = item_type
 
 class Trail(pygame.sprite.Sprite):
     def __init__(self, pos, image, groups):
         super().__init__(groups)
         self.image = image
         self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect    
+        self.hitbox = self.rect
+
+class Item(pygame.sprite.Sprite):
+    def __init__(self, pos, image, groups, item_type):
+        super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect
+        self.item_type = item_type
