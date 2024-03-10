76b0492693081ba05bdd83ce6304acc58d95aa15
Leopold R. <LEJM4@gmx.de>
2024-03-10 17:13:52 +0100

    hinzufuegen und implementierung der "esc_menu" logic --> funktioniert jetzt
    Was noch fehlt:
    
    i. "Game state manager" hinzufuegen, damit man variabler ist, was die "run methoden" der einzelnen Spiel "stati" betreffen

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pygame, sys
 from settings import SCREEN_HEIGHT, SCREEN_WIDTH
 from level import Level
 from data import Data   
-
+from esc_menu import EscMenu
 
 
 
@@ -18,14 +18,21 @@ class Game:
         pygame.display.set_caption('Time_traveller')
         self.clock = pygame.time.Clock()
 
+        #import stuff
         self.data = Data()
 
+        #import menu
+        self.esc_menu = EscMenu(self.screen)
+        self.esc_pressed = False
+        #states:
         self.level = Level(self.data)
 
 
-
+    def activate_esc_menu(self):
+        self.esc_menu.check_4_esc_menu_active()
 
     def run(self):
+        self.screen.fill((0,0,0))
 
         
         while True:
@@ -39,11 +46,29 @@ class Game:
                 if self.keys[pygame.K_BACKSPACE]:
                     pygame.quit()
                     sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.esc_pressed = not self.esc_pressed #macht immer das gegenteil von dem Zustand 
+                        self.activate_esc_menu()
+                        
+
+
+                if self.esc_menu.button_pressed_exit == True:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()                   
+
+                    
                 
             dt = self.clock.tick() / 1000
+            
             self.level.run(dt)
-            pygame.display.update()
 
+            if self.esc_pressed:
+                self.esc_menu.run()
+
+            pygame.display.update()
 
 
 if __name__ == '__main__':
