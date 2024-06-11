920a3a12fe18a4836da046672a64e0c68661227e
Mynameishose <www.masterofhose@gmail.com>
2024-06-11 21:03:38 +0200

    Zeitmaschine hinzugefuegt

---
+++ map/Tardis.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tardis" tilewidth="64" tileheight="64" tilecount="6" columns="2">
+ <image source="F:/BeLL project/On Map/Tardis.png" width="128" height="192"/>
+</tileset>
