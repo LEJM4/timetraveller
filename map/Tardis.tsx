da2daf79512fde953c5d610205c9580b2ecf758d
Mynameishose <www.masterofhose@gmail.com>
2024-06-11 21:27:35 +0200

    Tardis auf Map hinzugefuegt

--- map/Tardis.tsx
+++ map/Tardis.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tardis" tilewidth="64" tileheight="64" tilecount="6" columns="2">
- <image source="F:/BeLL project/On Map/Tardis.png" width="128" height="192"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Tardis" tilewidth="128" tileheight="192" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="192" source="F:/BeLL project/On Map/Tardis.png"/>
+ </tile>
 </tileset>
