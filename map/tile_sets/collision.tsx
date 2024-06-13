5833ca51b653fce61649cf02ad44ac89d31ab2f0
Mynameishose <www.masterofhose@gmail.com>
2024-06-13 15:46:40 +0200

    Umbenennung

--- map/tile_sets/collision.tsx
+++ map/tile_sets/collision.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Collision" tilewidth="64" tileheight="64" tilecount="4" columns="4">
- <image source="Collision.png" width="256" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="collision" tilewidth="64" tileheight="64" tilecount="4" columns="4">
+ <image source="../../graphics/objects/misc/collision.png" width="256" height="64"/>
 </tileset>
--- map/tile_sets/gras.tsx
+++ map/tile_sets/gras.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="gras" tilewidth="64" tileheight="64" tilecount="25" columns="5">
- <image source="gras.png" width="320" height="320"/>
+ <image source="../../graphics/ground/ground.png" width="320" height="320"/>
 </tileset>
