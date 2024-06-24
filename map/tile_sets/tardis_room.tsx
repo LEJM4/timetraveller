98ada639fabf8d717906a8f3e7be282d2eee4e50
Mynameishose <www.masterofhose@gmail.com>
2024-06-24 22:00:24 +0200

    Tardis innen hinzugefuegt

---
+++ map/tile_sets/tardis_room.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tardis_Map" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../../graphics/ground/Tardis_Map.png" width="512" height="512"/>
+</tileset>
