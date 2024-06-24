0b42178e9dc37c0ed9933efba71a3eb4a7a81df0
Mynameishose <www.masterofhose@gmail.com>
2024-06-24 22:19:46 +0200

    leos collisions sind nun besser

--- map/tile_sets/collision.tsx
+++ map/tile_sets/collision.tsx
@@ -1,4 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="collision" tilewidth="64" tileheight="64" tilecount="4" columns="4">
- <image source="../../graphics/objects/misc/collision.png" width="256" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="collision" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../graphics/objects/misc/collision_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="21" height="64" source="../../graphics/objects/misc/collision_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="20" source="../../graphics/objects/misc/collision_3.png"/>
+ </tile>
 </tileset>
