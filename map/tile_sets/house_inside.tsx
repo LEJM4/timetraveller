7d96972651787e876bd6b4e36920d78cda522877
Mynameishose <www.masterofhose@gmail.com>
2024-06-25 13:54:33 +0200

    haus inside hinzugefuegt

---
+++ map/tile_sets/house_inside.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="house_inside" tilewidth="128" tileheight="128" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="128" source="../../graphics/objects/misc/book_shelf.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="64" source="../../graphics/objects/misc/table.png"/>
+ </tile>
+</tileset>
